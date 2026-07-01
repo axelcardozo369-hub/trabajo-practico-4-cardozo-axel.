33e8b6caf24d74ca8297ca7628ae527b5d7558d8
axelcardozo369-hub <axelcardozo369@gmail.com>
2026-07-01 11:20:39 -0300

    se pudo verificar el CRUD ya se de crear mediante objectos cada pelicula, ya sea tambien en modificar, elimnar,ver por su id y tambien se modifico en la parte de data base que en el force estaba como true pero se cambio como false

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,17 @@
 import { Sequelize } from "sequelize";
-const sequelize = new sequelize(`movies`, `root`, ``, {
+const sequelize = new Sequelize("movies", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
+
+export const connectDB = () => {
+  sequelize
+    .sync({ force: false })
+    .then(() => {
+      console.log("Base de datos conectada y tabla creada");
+    })
+    .catch((error) => {
+      console.log("Error al conectar a la base de datos", error);
+    });
+};
 export default sequelize;
